7e3653ece1ec256104e88b8633d918c8ea05a529
unknown <duyskt228@gmail.com>
2021-12-16 13:59:35 +0700

    make it better

---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/banhang.css">
+    <link rel="stylesheet" href="/html/Trang2.html">
+    <script src="/js/web.js"></script>
+    <title>Thế Giới di động</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <a href="https://www.thegioididong.com/"><img src ="/image/Logo1.jpg" alt=""></a>
+        </header>
+        <nav>
+               <div class="menu">
+                     <ul>
+                         <li><a href="https://www.thegioididong.com/dtdd">Điện thoại </a></li>
+                         <li><a href="https://www.thegioididong.com/laptop-ldp">Laptop</a></li>
+                         <li><a href="https://www.thegioididong.com/dong-ho-thong-minh-ldp">Đồng Hồ Thông Minh</a></li>
+                         <li><a href="https://www.thegioididong.com/dong-ho">Đồng Hồ Thời Trang</a></li>
+                         <li><a href="https://www.thegioididong.com/sim-so-dep">Sim</a></li>
+                         <li><a href="https://www.thegioididong.com/sim-so-dep">Thẻ cào</a></li>
+                     </ul>
+               </div>
+               <div class="search">
+
+                      <input type="search" blaceholder="search...">
+                      <button>Tìm kiếm</button>
+               </div>
+    
+</nav>
+         <div id="slide">
+             <a href="https://www.thegioididong.com/khuyen-mai-soc/series-a"><img src="/image/Hinh0.png" alt=" " ></a>
+         </div>
+         <div id="hot">
+             <a href="https://www.thegioididong.com/dtdd-apple-iphone-12-series"><img src="/image/Hinh0.1.jpg" alt=""></a>
+         </div>
+         <div id="hot1">
+             <a href="https://www.thegioididong.com/samsung"><img src="/image/Hinh0.2.jpg" alt=""></a>
+         </div>
+         <div id="hot2">
+            <a href="https://www.thegioididong.com/tai-nghe-apple-khong-day#c=54&m=2660&p=148560&ext=sp2020&o=9&pi=0"><img src="/image/Hinh0.3.jpg" alt=""></a>
+         </div>
+         <div id="hot3">
+            <a href="https://www.thegioididong.com/laptop-gaming?itm_source=homepage&itm_medium=hot_banner&itm_campaign=internal_tracking&itm_content=desktop_1548"><img src="/image/Hinh0.4.jpg" alt=""></a>
+         </div>
+         <div id="hot4">
+            <a href="https://www.thegioididong.com/laptop-gaming?itm_source=homepage&itm_medium=hot_banner&itm_campaign=internal_tracking&itm_content=desktop_1548"><img src="" alt=""></a>
+         </div>
+         <div id="hot5">
+            <a href="/html/Trang2.html"><img src="/image/Slide2.jpg" alt=""></a>
+         </div>
+         <h1>ĐIỆN THOẠI NỔI BẬT NHẤT</h1>
+    <article>
+           <div class="banno">
+               <div id="al1">
+                   <a href=""><img src="/image/Hinh2.jpg" alt=""></a>
+               </div>
+               <div id="al2">
+                <a href=""><img src="/image/Hinh1.jpg" alt=""></a>
+            </div>
+            <div id="al3">
+                <a href=""><img src="/image/Hinh3.jpg" alt=""></a>
+            </div>
+            <div id="al4">
+                <a href=""><img src="/image/Hinh4.jpg" alt=""></a>
+            </div>
+            <div id="alo5">
+                <a href=""><img src="/image/Hinh5.jpg" alt=""></a>
+             </div>
+             <div id="alo6">
+                <a href=""><img src="/image/Hinh6.jpg" alt=""></a>
+             </div>
+             <div id="alo7">
+                <a href=""><img src="/image/Hinh7.jpg" alt=""></a>
+             </div>
+             <div id="alo8">
+                <a href=""><img src="/image/Hinh8.jpg" alt=""></a>
+             </div>
+         <div>
+             <div id="Khon">
+                 <a href="https://www.thegioididong.com/dtdd/iphone-13"><img src="/image/Mua Ngay.jpg" alt=""></a>
+             </div>
+             <div id="Khon2">
+                <a href="https://www.thegioididong.com/dtdd/oppo-reno6-pro-5g?src=osp"><img src="/image/Mua Ngay.jpg" alt=""></a>
+            </div>
+            <div id="Khon3">
+                <a href="https://www.thegioididong.com/dtdd/iphone-11?src=osp"><img src="/image/Mua Ngay.jpg" alt=""></a>
+            </div>
+            <div id="Khon4">
+                <a href="https://www.thegioididong.com/dtdd/samsung-galaxy-z-flip-3?src=osp"><img src="/image/Mua Ngay.jpg" alt=""></a>
+            </div><div id="Khon5">
+                <a href="https://www.thegioididong.com/dtdd/xiaomi-11t-256gb"><img src="/image/Mua Ngay.jpg" alt=""></a>
+            </div>
+        </div><div id="Khon6">
+            <a href="https://www.thegioididong.com/dtdd/xiaomi-11t-256gb"><img src="/image/Mua Ngay.jpg" alt=""></a>
+        </div>
+    </div><div id="Khon7">
+        <a href="https://www.thegioididong.com/dtdd/xiaomi-11t-256gb"><img src="/image/Mua Ngay.jpg" alt=""></a>
+    </div>
+    <div id="Khon8">
+        <a href=""><img src="/image/Mua Ngay.jpg" alt=""></a>
+         </div>
+    </article>
+            <h2>LAPTOP, TABLET NỔI BẬT NHẤT</h2>
+            <div>
+                <div id="Ngu">
+                    <a href=""><img src="/image2/Hinh1.jpg" alt=""></a>
+                </div>
+                <div id="Hay">
+                <a href="https://www.thegioididong.com/laptop/msi-gf66-11uc-i7-224vn">
+                    <p>Laptop MSI Katana Gaming GF66</p>
+                </a>
+            </div>
+                <div id="Ngu2">
+                    <a href=""><img src="/image2/Hinh2.jpg" alt=""></a>
+                </div>
+                <div id="Hay2">
+                    <a href="https://www.thegioididong.com/laptop/msi-gf66-11uc-i7-224vn">
+                        <p>Laptop MSI Gaming Pulse GL66</p>
+                    </a>
+                <div id="Ngu3">
+                    <a href=""><img src="/image2/Hinh3.jpg" alt=""></a>
+                </div>
+                <div id="Hay3">
+                    <a href="https://www.thegioididong.com/laptop/msi-gf66-11uc-i7-224vn">
+                        <p>Laptop Gigabyte Gaming G5 i5 10500H</p>
+                    </a>
+                </div><div id="Khon9">
+                    <a href="https://www.thegioididong.com/laptop/msi-gf66-11uc-i7-224vn"><img src="/image/Mua Ngay.jpg" alt=""></a>
+                </div>
+            </div><div id="Khon10">
+                <a href="https://www.thegioididong.com/laptop/msi-gf66-11uc-i7-224vn"><img src="/image/Mua Ngay.jpg" alt=""></a>
+            </div>
+        </div><div id="Khon11">
+            <a href="https://www.thegioididong.com/laptop/msi-gf66-11uc-i7-224vn"><img src="/image/Mua Ngay.jpg" alt=""></a>
+        </div><div id="Khon12">
+            <a href="https://www.thegioididong.com/laptop/macbook-pro-14-m1-pro-2021-8-core-cpu"><img src="/image/Mua Ngay.jpg" alt=""></a>
+        </div>
+            </div>
+    <article>
+        <div>
+        <div id="Ngu4">
+            <a href=""><img src="/image2/Hinh4.jpg" alt=""></a>
+        </div>
+        <div id="Hay4">
+        <a href="https://www.thegioididong.com/laptop/macbook-pro-14-m1-pro-2021-8-core-cpu">
+            <p>Laptop MacBook Pro 14 M1 Pro 2021</p>
+        <div id="Ngu5">
+            <a href=""><img src="/image2/Hinh5.jpg" alt=""></a>
+        </div>
+        <div id="Hay5">
+        <a href="https://www.thegioididong.com/laptop/macbook-pro-14-m1-pro-2021-8-core-cpu">
+            <p>Laptop Acer Nitro 5 Gaming</p>
+        </a>
+    </div><div id="Khon13">
+        <a href="https://www.thegioididong.com/laptop/acer-nitro-gaming-an515-57-727j-i7-nhqd9sv005#top-featured-images-gallery-0"><img src="/image/Mua Ngay.jpg" alt=""></a>
+    </div>
+    <div id="Ngu6">
+        <a href=""><img src="/image2/Hinh6.jpg" alt=""></a>
+    </div>
+    <div id="Hay6">
+    <a href="https://www.thegioididong.com/laptop/macbook-pro-14-m1-pro-2021-8-core-cpu">
+        <p>Laptop Acer Nitro 5 Gaming AN515 57</p>
+    </div>
+</div><div id="Khon14">
+    <a href="https://www.thegioididong.com/laptop/macbook-pro-14-m1-pro-2021-8-core-cpu"><img src="/image/Mua Ngay.jpg" alt=""></a>
+</div>
+</div>
+    </article>
+    <article>
+        <div>
+          <h3>PHỤ KIỆN NỔI BẬT NHẤT</h3>
+          <div id="Ngu7">
+            <a href=""><img src="/image3/Hinh1.jpg" alt=""></a>
+        </div>
+        <div id="Hay7">
+        <a href="https://www.thegioididong.com/tai-nghe/bluetooth-true-wireless-galaxy-buds-pro-bac">
+            <p>Tai nghe Bluetooth True</p>
+            <div id="Ngu8">
+                <a href=""><img src="/image3/Hinh2.jpg" alt=""></a>
+            </div>
+            <div id="Hay8">
+            <a href="https://www.thegioididong.com/tai-nghe/bluetooth-true-wireless-hydrus-ts12bc">
+                <p>Bàn Phím Cơ Có Dây Gaming</p>
+                <div id="Ngu9">
+                    <a href=""><img src="/image3/Hinh3.jpg" alt=""></a>
+                </div>
+                <div id="Hay9">
+                <a href="https://www.thegioididong.com/sac-dtdd/sac-du-phong-polymer-10000mah-type-c-p66d-xam">
+                    <p>Pin sạc dự phòng không dây Polymer 10.000 mAh </p>
+                </div>
+                <div id="Khon15">
+                    <a href="https://www.thegioididong.com/tai-nghe/bluetooth-true-wireless-galaxy-buds-pro-bac"><img src="/image/Mua Ngay.jpg" alt=""></a>
+                </div>
+            </div><div id="Khon16">
+                <a href="https://www.thegioididong.com/tai-nghe/bluetooth-true-wireless-hydrus-ts12bc"><img src="/image/Mua Ngay.jpg" alt=""></a>
+            </div>
+        </div><div id="Khon17">
+            <a href="https://www.thegioididong.com/sac-dtdd/sac-du-phong-polymer-10000mah-type-c-p66d-xam"><img src="/image/Mua Ngay.jpg" alt=""></a>
+        </div>
+        <div id="Hay10">
+            <a href="https://www.thegioididong.com/lich-su-mua-hang/dang-nhap">
+                <p>Lịch Sử Mua Hàng</p>
+        </div>
+        <div id="Hay11">
+            <a href="https://www.thegioididong.com/daily">
+                <p>Cộng tác bán hàng cùng thế giới di động</p>
+        </div>
+        <div id="Hay12">
+            <a href="https://www.thegioididong.com/tra-gop">
+                <p>Tìm hiểu về mua trả góp</p>
+        </div>
+        <div id="Hay13">
+            <a href="https://www.thegioididong.com/bao-hanh">
+                <p>Chính sách bảo mật</p>
+        </div>
+        <div id="Hay14">
+            <a href="https://mwg.vn/">
+                <p>Giới thiệu công ty</p>
+        </div>
+        <div id="Hay15">
+            <a href="https://vieclam.thegioididong.com/">
+                <p>Tuyển Dụng</p>
+        </div>
+        <div id="Hay16">
+            <a href="https://www.thegioididong.com/lien-he">
+                <p>Gửi Góp ý Khiếu nại</p>
+        </div>
+        <div id="Hay17">
+            <a href="https://www.thegioididong.com/lien-he">
+                <p>Tìm Siêu Thị</p>
+        </div>
+        <div id="Hay18">
+            <a href="">
+                <p>Tổng Đài Hỗ Trợ ( Miễn Phí Gọi )</p>
+        </div>
+        <div id="Hay19">
+            <a href="">
+                <p>Gọi Mua  : </p>
+        </div>
+        <div id="Hay20">
+            <a href="">
+                <p>Kỹ Thuật  : </p>
+        </div>
+        <div id="Hay21">
+            <a href="">
+                <p>Khiếu nại  : </p>
+        </div>
+        <div id="Hay22">
+            <a href="">
+                <p> 1800.1060 </p>
+        </div>
+        <div id="Hay23">
+            <a href="">
+                <p> 1800.1763 </p>
+        </div>
+        <div id="Hay24">
+            <a href="">
+                <p> 1800.1062 </p>
+        </div>
+        <div id="Hay25">
+            <a href="">
+                <p>( 7:30-22:00 )</p>
+        </div>
+        <div id="Hay26">
+            <a href="">
+                <p>( 7:30-22:00 )</p>
+        </div>
+        <div id="Hay27">
+            <a href="">
+                <p>( 7:30-21:30 )</p>
+        </div>
+        <div id="Hay28">
+            <a href=""><img src="/image3/Hinh5.png" alt=""></a>
+        </div>
+        <div id="Hay29">
+            <a href=""><img src="/image3/Hinh6.png" alt=""></a>
+        </div>
+        <div id="Hay30">
+            <a href=""><img src="/image4/Hinh1.jpg" alt=""></a>
+        </div>
+        <div id="Hay31">
+            <a href=""><img src="/image4/Hinh2.jpg" alt=""></a>
+        </div>
+        <div id="Hay32">
+            <a href=""><img src="/image4/Hinh3.1.jpg" alt=""></a>
+        </div>
+        <h4><b>Web Site cùng các tập đoàn lớn , Nhà Tài Trợ </b> </h4>
+    </article>
+<article>
+    <footer>
+        Mọi Thông tin liên hệ xin gọi sđt 0827.157.729 Nguyễn Đức Duy HPC CNT-02
+    </footer>
+</body>
+</html>
